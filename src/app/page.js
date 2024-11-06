b15c28d69f8055b141bf2f4cf629e83aece4583f
prajjwalsoni123 <prajjwalsoni123@gmail.com>
2024-11-06 19:20:22 +0530

    first commit

--- src/app/page.js
+++ src/app/page.js
@@ -1,95 +1,84 @@
+"use client"
 import Image from "next/image";
 import styles from "./page.module.css";
+import axios from "axios";
+import { useEffect, useState } from "react";
 
 export default function Home() {
-  return (
-    <div className={styles.page}>
-      <main className={styles.main}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol>
-          <li>
-            Get started by editing <code>src/app/page.js</code>.
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
 
-        <div className={styles.ctas}>
-          <a
-            className={styles.primary}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className={styles.logo}
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.secondary}
-          >
-            Read our docs
-          </a>
+  const [formData, setFormData] = useState({});
+
+
+  console.log("FormData");
+  console.log(formData);
+
+
+  const onLoginSubmit = async () => {
+      const { email, username, password } = formData || {};
+
+      console.log(formData);
+      
+      try {
+        const res = await axios.post(`http://localhost:8080/api/signup`, {
+          email,
+          username,
+          password
+        });
+
+        console.log(res, "response");
+
+      }
+      catch (err) {
+         console.log(err, "error");
+      }
+   
+  }
+
+  return (
+    <div className="main-container">
+        <div className="main-navbar">
+          <div>
+             Signup
+          </div>
+          <div>
+            Login
+          </div>
+          <div>
+            Logout
+          </div>
         </div>
-      </main>
-      <footer className={styles.footer}>
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+        <div className="login-container">
+           <div>
+             Login in Social media Fuzzbook
+           </div>
+           <div>
+             <div>
+                Email
+             </div>
+             <input type='text' value={formData.email || ''} name='email' onChange={(e) => {
+                 setFormData((val) => ({ ...val,email: e.target.value }));
+             }}/>
+           </div>
+           <div>
+             <div>
+                Name
+             </div>
+             <input type='text' value={formData.name || ''} name='username' onChange={(e) => {
+                setFormData((val) => ({ ...val, name: e.target.value }))
+             }}/>
+           </div>
+           <div>
+             <div>
+                Password
+             </div>
+             <input type='text' value={formData.password || ''} name='password' onChange={(e) => {
+                setFormData((val) => ({ ...val, password: e.target.value }));
+             }}/> 
+           </div>
+           <button onClick={() => { onLoginSubmit() }}>
+              Submit 
+           </button>
+        </div>
     </div>
   );
 }
