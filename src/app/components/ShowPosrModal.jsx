e98d8106e6604589c09b43d13a58f4070d88d964
prajjwalsoni123 <prajjwalsoni123@gmail.com>
2024-11-21 14:57:19 +0530

    post modal addition

--- src/app/components/ShowPosrModal.jsx
+++ src/app/components/ShowPosrModal.jsx
@@ -1,6 +1,8 @@
 import axios from "axios";
 
 import { useState } from "react";
+import messageI from '../../../public/profile/message.svg';
+import likeI from '../../../public/profile/like.svg';
 
 
 const ShowPostModal = ({ postObj, setShowPost }) => {
@@ -16,8 +18,21 @@ const ShowPostModal = ({ postObj, setShowPost }) => {
      <>
         <div className="comment-modal-wrapper"></div>
         <div className="comment-modal-container">  
-           Hello Show Post
-       <button className="model-btn" onClick={() => { setShowPost(false);  }}>Close it</button>
+           <div className="post-content-container">
+           Post - 
+           <div className="post-main-container">{postObj.message}</div>
+           <div className="post-action-container">
+           <div className="post-action-image">
+             {postObj?.comments.length}
+             <img style={{ cursor: 'pointer' }} src={messageI.src} width={'20px'} height={'20px'}/>
+           </div>
+           <div className="post-action-image">
+             {postObj?.likes.length}
+             <img style={{ cursor: 'pointer' }} src={likeI.src} width={'20px'} height={'20px'}/>
+           </div>
+           </div>
+           <button className="model-btn" onClick={() => { setShowPost(false);  }}>Close it</button>
+           </div>
        </div>
       </>
     )
