6a051b52fb1cabc4697b1dcd52a7235f3f54ed9b
prajjwalsoni123 <prajjwalsoni123@gmail.com>
2024-11-22 13:25:23 +0530

    analytics dashbard ui creation

--- src/app/analytics/[id]/page.jsx
+++ src/app/analytics/[id]/page.jsx
@@ -1,8 +1,21 @@
-import React from 'react'
+"use client";
+import { useEffect } from "react"
+import { useState } from "react"
 
 function page() {
   return (
-    <div>page</div>
+    <div className="dashboard-container">
+        <div className="analytics-container">
+        <div className="analytics-component">
+        <div> Total Visits:</div>
+        <div> 23</div>
+        </div>
+        <div className="analytics-component">
+        <div> See who visits your profile in last 5 mins:</div>
+        <div> Click here</div>
+        </div>
+        </div>
+    </div>
   )
 }
 
