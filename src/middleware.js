0c50a4d6bca45b835bf9d0e6a5a34ca3fce54b82
prajjwalsoni123 <prajjwalsoni123@gmail.com>
2024-11-27 20:41:26 +0530

    cookies addition

--- src/middleware.js
+++ src/middleware.js
@@ -6,6 +6,8 @@ import Cookies from 'js-cookie';
 export function middleware(request) {
   
   const refreshToken = request.cookies.get('refreshToken');
+  console.log('middleware refreshToken');
+  console.log(refreshToken);
   if (!refreshToken) {
      return NextResponse.redirect(new URL('/', request.url))
   }
